7a8fa94c5cf65fd79c282c696ccc95213891ee6c
jino.michel.aque <contact@geeklogbook.com>
2022-10-07 12:43:26 -0300

    add animation

---
+++ 07.split_landing/script.js
@@ -0,0 +1,9 @@
+const left = document.querySelector('.left')
+const rigth = document.querySelector('.right')
+const container = document.querySelector('.container')
+
+left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
+left.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
+
+rigth.addEventListener('mouseenter', () => container.classList.add('hover-right'))
+rigth.addEventListener('mouseleave', () => container.classList.remove('hover-right'))
