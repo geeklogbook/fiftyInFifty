848627906378d61b5266009fecd58b3f6142264c
jino.michel.aque <contact@geeklogbook.com>
2023-01-10 23:49:22 -0300

    add finish of style and the play button

--- projects/50.insect_catch_game/script.js
+++ projects/50.insect_catch_game/script.js
@@ -1,3 +1,13 @@
 const screens = document.querySelectorAll('.screen')
+const choose_insect_butns = document.querySelectorAll('.choose-insect-btn')
+const start_btn = document.getElementById('start-btn')
+const timeEl = document.getElementById('time')
+const scoreEl = document.getElementById('score')
+const message = document.getElementById('message')
+const game_container = document.getElementById('.game_container')
 
-screens[0].classList.add('up')
+let seconds = 0
+let score = 0
+let selected_insect = {}
+
+start_btn.addEventListener('click', () => screens[0].classList.add('up'))
