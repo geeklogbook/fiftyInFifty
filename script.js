9b1b738b6e8b99ab1320e9423e44a2817a13f430
jino.michel.aque <contact@geeklogbook.com>
2023-01-15 19:44:12 -0300

    add style changes

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ const getProjects = async (idx) => {
 
 const createProjectCard = (project) => {
     const projectEl = document.createElement('div')
+    projectEl.classList.add("project")
 
     const id = project.id
     const name = project.name
@@ -24,18 +25,19 @@ const createProjectCard = (project) => {
 
 
     const projectInnerHTML = `
-    <div class="card">
         <div class="img-container">
             <img src= ${image}/${id} alt="" />
         </div>
-    <h3>${name}</h3>
-        <p>This project expand cards</p>
-    <button> 
-        <a href="./projects/${id}.${location}/index.html" class="href" >Go to the project</a>
-    </button>
-        <p>Tags: Widget, Work With Images, HTML, CSS, JS</p>
-    <p>Reutilizable: yes</p>
-    </div>
+        <div class="info">
+            <h2>${name}</h2>
+            <p>This project expand cards</p>
+            <p>Tags: Widget, Work With Images, HTML, CSS, JS</p>
+            <p>Reutilizable: Yes - Review: No </p>
+        </div>
+
+        <button> 
+            <a href="./projects/${id}.${location}/index.html" class="href" >Go to the project</a>
+        </button>
     `
     projectEl.innerHTML = projectInnerHTML
     project_container.appendChild(projectEl)
