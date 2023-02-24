6343261dd829032abe85f71a743cceaa7a172d05
jino.michel.aque <contact@geeklogbook.com>
2023-02-23 23:51:22 -0300

    change format and add first blog post

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const project_container = document.getElementById('projects-container')
 const projects_count = 50
 
 const fetchProjects = async () => {
-    for (let i = 1; i < projects_count; i++) {
+    for (let i = 0; i < projects_count; i++) {
         await getProjects(i)
     }
 }
@@ -22,21 +22,22 @@ const createProjectCard = (project) => {
     const name = project.name
     const location = project.location
     const image = project.image
+    const reusable = project.reusable
+    const url = project.url
 
 
     const projectInnerHTML = `
         <div class="img-container">
-            <img src= ${image}/${id} alt="" />
+            <img src= ${image} alt="" />
         </div>
         <div class="info">
             <h2>${name}</h2>
-            <p>This project expand cards</p>
-            <p>Tags: Widget, Work With Images, HTML, CSS, JS</p>
-            <p>Reutilizable: Yes - Review: No </p>
+            <p> Reusable: ${reusable} </p>
+            <p> <a href=${url} target="_blank"> Review </a> </p>
         </div>
 
         <button> 
-            <a href="./projects/${id}.${location}/index.html" class="href" >Go to the project</a>
+            <a href="./projects/${id}.${location}/index.html" class="href">Go to the project</a>
         </button>
     `
     projectEl.innerHTML = projectInnerHTML
