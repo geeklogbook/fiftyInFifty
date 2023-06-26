78382a3cbef78fd3e0f67b31d48904c2e05b0dc0
jino.michel.aque <contact@geeklogbook.com>
2023-06-26 14:52:49 -0300

    fix style problems

--- script.js
+++ script.js
@@ -1,47 +1,46 @@
-const project_container = document.getElementById('projects-container')
-
-const projects_count = 50
+const projectContainer = document.getElementById('projects-container');
 
 const fetchProjects = async () => {
-    for (let i = 0; i < projects_count; i++) {
-        await getProjects(i)
-    }
-}
-
-const getProjects = async (idx) => {
-    const res = await fetch('projects.json')
-    const data = await res.json()
-    createProjectCard(data[idx])
-}
+  try {
+    const res = await fetch('projects.json');
+    const data = await res.json();
+    data.forEach(createProjectCard);
+  } catch (error) {
+    console.error('Error fetching projects:', error);
+  }
+};
 
 const createProjectCard = (project) => {
-    const projectEl = document.createElement('div')
-    projectEl.classList.add("project")
+  const projectEl = createProjectElement(project);
+  appendProjectElement(projectEl);
+};
 
-    const id = project.id
-    const name = project.name
-    const location = project.location
-    const image = project.image
-    const reusable = project.reusable
-    const url = project.url
+const createProjectElement = (project) => {
+  const { id, name, location, image, reusable, url } = project;
+  const projectEl = document.createElement('div');
+  projectEl.classList.add('project');
 
+  const projectInnerHTML = `
+    <div class="img-container">
+      <img src="${image}" alt="" />
+    </div>
+    <div class="info">
+      <h3>${name}</h3>
+      <p>Reusable: ${reusable}</p>
+      <p><a href="${url}" target="_blank">Review</a></p>
+    </div>
 
-    const projectInnerHTML = `
-        <div class="img-container">
-            <img src= ${image} alt="" />
-        </div>
-        <div class="info">
-            <h2>${name}</h2>
-            <p> Reusable: ${reusable} </p>
-            <p> <a href=${url} target="_blank"> Review </a> </p>
-        </div>
+    <button>
+      <a href="./projects/${id}.${location}/index.html" class="href" target="_blank">Go to the project</a>
+    </button>
+  `;
 
-        <button> 
-            <a href="./projects/${id}.${location}/index.html" class="href" target="_blank">Go to the project</a>
-        </button>
-    `
-    projectEl.innerHTML = projectInnerHTML
-    project_container.appendChild(projectEl)
-}
+  projectEl.innerHTML = projectInnerHTML;
+  return projectEl;
+};
 
-fetchProjects()
+const appendProjectElement = (projectEl) => {
+  projectContainer.appendChild(projectEl);
+};
+
+fetchProjects();
